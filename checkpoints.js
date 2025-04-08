aef72bcc1f421a84f31f308afc904bf897cf2597
QiChen212221012 <164645315+QiChen212221012@users.noreply.github.com>
2025-04-08 17:29:06 +0100

    Save local changes before pull

---
+++ checkpoints.js
@@ -0,0 +1,22 @@
+// checkpoints.js
+export const checkpoints = [
+    {
+      id: 'library',
+      name: 'UWS Paisley Campus Library',
+      latitude: 55.84363,
+      longitude: -4.42792,
+    },
+    {
+      id: 'campus',
+      name: 'Paisley Campus (UWS)',
+      latitude: 55.84417,
+      longitude: -4.43030,
+    },
+    {
+      id: 'glasgow',
+      name: 'Glasgow City',
+      latitude: 55.86306,
+      longitude: -4.24208,
+    },
+  ];
+  
